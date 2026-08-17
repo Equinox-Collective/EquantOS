97b12ff1cf9abe83af949dfee0a0a09888e85cba
ewasion137 <formanban94@gmail.com>
2026-08-17 18:53:52 +0300

    better pmm.h
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- src/kernel/core/mem/pmm.h
+++ src/kernel/core/mem/pmm.h
@@ -1,18 +1,34 @@
+// src/kernel/core/mem/pmm.h
 #ifndef PMM_H
 #define PMM_H
 
 #include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 
-// Экспортируем переменные, чтобы kernel.c их видел s
-extern uint64_t free_memory;
-extern uint64_t total_pages;
+#define PAGE_SIZE 4096ULL
+#define PMM_MAX_ORDER 11 // Orders 0..10 (Up to 4MB continuous blocks)
 
-// Функции PMM
+typedef struct pmm_page {
+    uint32_t flags;
+    uint8_t order;
+    bool is_free;
+    struct pmm_page *next;
+    struct pmm_page *prev;
+} pmm_page_t;
+
+typedef struct {
+    pmm_page_t *freelist;
+    size_t free_count;
+} pmm_free_area_t;
+
 void pmm_init(void);
-void *pmm_alloc(void);
-void *pmm_alloc_continuous(uint64_t count);
-void pmm_free(void *ptr);
-extern uint64_t pmm_get_used_memory();
-extern uint64_t pmm_get_total_memory();
+void *pmm_alloc_pages(size_t order);
+void *pmm_alloc(void); // Order 0 helper
+void pmm_free_pages(void *ptr, size_t order);
+void pmm_free(void *ptr); // Order 0 helper
 
-#endif
+uint64_t pmm_get_used_memory(void);
+uint64_t pmm_get_total_memory(void);
+
+#endif // PMM_H
