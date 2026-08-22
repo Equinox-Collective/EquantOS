2845826a1ea9a7a7e985932b37f7731a9f18f9d7
ewasion137 <formanban94@gmail.com>
2026-08-22 18:29:07 +0300

    USB.c
    Signed-off-by: ewasion137 <formanban94@gmail.com>

---
+++ src/kernel/drivers/usb/usb.c
@@ -0,0 +1,12 @@
+// src/kernel/drivers/usb/usb.c - Core USB Request Helper Functions
+#include "usb.h"
+#include "../serial/serial.h"
+
+void usb_build_setup_packet(usb_setup_packet_t *pkt, uint8_t req_type, uint8_t req, uint16_t val, uint16_t idx, uint16_t len) {
+    if (!pkt) return;
+    pkt->request_type = req_type;
+    pkt->request      = req;
+    pkt->value        = val;
+    pkt->index        = idx;
+    pkt->length       = len;
+}
