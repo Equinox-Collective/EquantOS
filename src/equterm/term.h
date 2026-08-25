14dd5b656136201d78d39fc22ba3a7ad929abb2b
ewasion137 <formanban94@gmail.com>
2026-08-25 20:55:21 +0300

    new term functions
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- src/equterm/term.h
+++ src/equterm/term.h
@@ -1,4 +1,3 @@
-// src/equterm/term.h - Clean Display Interface
 #ifndef TERM_H
 #define TERM_H
 
@@ -9,11 +8,15 @@ void term_init(void *fb_addr, uint64_t width, uint64_t height, uint64_t pitch);
 void term_putchar_raw(char c);
 void term_clear_screen(void);
 
-// Standard API Wrappers forwarded to TTY
 void term_putchar(char c);
 void term_print(const char *str);
 void term_clear(void);
 void term_set_color(uint32_t color);
+void term_set_custom_colors(uint32_t fg, uint32_t bg);
 uint32_t term_get_color(void);
+
+size_t term_get_cursor_x(void);
+size_t term_get_cursor_y(void);
+void term_set_cursor(size_t x, size_t y);
 
 #endif // TERM_H
