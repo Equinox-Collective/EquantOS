a058b816688a8fb125c52d8e96c187cb5a4326a8
ewasion137 <formanban94@gmail.com>
2026-08-25 21:09:13 +0300

    new partition funcs
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- src/kernel/fs/partition.h
+++ src/kernel/fs/partition.h
@@ -1,21 +1,45 @@
-// partition.h - Unified Partition Table Management (MBR & GPT abstraction)
+// partition.h - Advanced Hardware Partition & Free Space Scanner
 #ifndef PARTITION_H
 #define PARTITION_H
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "../drivers/disk/block.h"
 
+#define MAX_TOTAL_PARTITIONS 32
+
+typedef enum {
+    PARTITION_TYPE_UNKNOWN = 0,
+    PARTITION_TYPE_FAT32,
+    PARTITION_TYPE_EXT2,
+    PARTITION_TYPE_ESP,      // EFI System Partition (Protect against accidental format)
+    PARTITION_TYPE_UNALLOCATED
+} partition_kind_t;
+
 typedef struct {
+    uint8_t index;
     uint32_t start_lba;
     uint32_t sector_count;
-    uint8_t  type;
-    uint8_t  index;
+    uint8_t raw_type;
+    partition_kind_t kind;
+    char fs_name[32];
+    bool is_esp;             // Critical Hardware Protection Guard
 } partition_info_t;
 
-// Unified disk partition scanner interface
+typedef struct {
+    char name[32];
+    block_device_t bdev;
+    uint64_t total_sectors;
+    uint32_t sector_size;
+    partition_info_t partitions[MAX_TOTAL_PARTITIONS];
+    int partition_count;
+} disk_device_info_t;
+
 void disk_partition_scan_device(block_device_t dev);
-void disk_partition_scan(uint8_t drive);
 int disk_get_partition_count(void);
 partition_info_t *disk_get_partition(int index);
+
+// Free Space Analysis
+void disk_find_unallocated_space(disk_device_info_t *disk);
 
 #endif // PARTITION_H
