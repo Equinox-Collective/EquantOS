98669faf7c7868d41012c25421d226a5fefaec7f
ewasion137 <formanban94@gmail.com>
2026-08-25 17:38:08 +0300

    installer.h
    Signed-off-by: ewasion137 <formanban94@gmail.com>

---
+++ src/kernel/misc/installer.h
@@ -0,0 +1,11 @@
+// src/kernel/misc/installer.h - Arch-style Interactive EquantOS Installer
+#ifndef INSTALLER_H
+#define INSTALLER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// Launches the interactive kernel installation wizard
+int run_installer(void);
+
+#endif // INSTALLER_H
