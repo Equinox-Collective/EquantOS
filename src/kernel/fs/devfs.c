664459154dd2c0435e09e848421f6be745a5d54f
ewasion137 <formanban94@gmail.com>
2026-09-05 11:02:01 +0300

    fb0 in devfs
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- src/kernel/fs/devfs.c
+++ src/kernel/fs/devfs.c
@@ -8,8 +8,15 @@
 #include "../drivers/input.h"
 #include "../../equterm/term.h"
 #include "../core/initcall.h"
+#include "../../limine.h"
+#include "../core/mem/vmm.h"
+#include "../core/mem/pmm.h"
+#include "../proc/task.h"
+#include "../proc/syscall.h"
 
 static vfs_node_t *devfs_root = NULL;
+extern struct limine_framebuffer *kernel_fb;
+extern uint64_t hhdm_offset;
 
 // Handlers for /dev/null
 static int64_t dev_null_read(vfs_node_t *node, uint64_t offset, uint64_t size, uint8_t *buffer) {
@@ -111,6 +118,49 @@ static vfs_file_operations_t devfs_root_fops = {
     .finddir = devfs_finddir
 };
 
+static int dev_fb_ioctl(vfs_node_t *node, uint64_t req, void *arg) {
+    (void)node;
+    if (!kernel_fb || !arg) return -EINVAL;
+
+    if (req == FBIOGET_VSCREENINFO) {
+        struct fb_var_screeninfo *var = (struct fb_var_screeninfo *)arg;
+        var->xres = kernel_fb->width;
+        var->yres = kernel_fb->height;
+        var->bits_per_pixel = kernel_fb->bpp;
+        return 0;
+    }
+    return -ENOTTY;
+}
+
+static int64_t dev_fb_mmap(vfs_node_t *node, uint64_t addr, size_t length, int prot, int flags, int64_t offset) {
+    (void)node; (void)prot; (void)flags; (void)offset;
+    if (!kernel_fb) return -ENODEV;
+    if (!current_task || !current_task->process) return -EINVAL;
+
+    // Convert Limine HHDM virtual framebuffer address to raw physical address
+    uint64_t fb_phys = (uint64_t)kernel_fb->address - hhdm_offset;
+    uint32_t total_size = kernel_fb->pitch * kernel_fb->height;
+    if (length > total_size) length = total_size;
+
+    size_t page_count = (length + PAGE_SIZE - 1) / PAGE_SIZE;
+    page_table_t *pml4 = (page_table_t *)VIRT(current_task->process->cr3);
+
+    // Map physical video memory directly into user virtual space (Write-Combining / Uncached)
+    for (size_t i = 0; i < page_count; i++) {
+        vmm_map(pml4, addr + (i * PAGE_SIZE), fb_phys + (i * PAGE_SIZE),
+                PTE_PRESENT | PTE_WRITABLE | PTE_USER | PTE_PCD | PTE_PWT);
+    }
+
+    return (int64_t)addr;
+}
+
+static vfs_file_operations_t fb_fops = {
+    .read = NULL,
+    .write = NULL,
+    .ioctl = dev_fb_ioctl,
+    .mmap = dev_fb_mmap
+};
+
 void devfs_init(void) {
     devfs_root = (vfs_node_t *)kzalloc(sizeof(vfs_node_t));
     strcpy(devfs_root->name, "dev");
@@ -121,6 +171,7 @@ void devfs_init(void) {
     devfs_register_device("null", &null_fops, NULL, 0);
     devfs_register_device("input0", &input_fops, NULL, 0);
     devfs_register_device("tty0", &tty_fops, NULL, 0);
+    devfs_register_device("fb0", &fb_fops, NULL, 0);
 
     // Mount /dev onto VFS root
     if (vfs_root) {
