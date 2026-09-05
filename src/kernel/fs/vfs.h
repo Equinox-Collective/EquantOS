a781b36c6109e6300304dd0234f632c27e17c5b5
ewasion137 <formanban94@gmail.com>
2026-09-05 10:58:56 +0300

    Framebf physical adress and ioctl/mmap in VFS
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- src/kernel/fs/vfs.h
+++ src/kernel/fs/vfs.h
@@ -8,8 +8,23 @@
 #define FS_FILE        0x01
 #define FS_DIRECTORY   0x02
 #define FS_MOUNTPOINT  0x08
+#define FBIOGET_VSCREENINFO 0x4600
 
 struct vfs_node;
+
+struct fb_fix_screeninfo {
+    uint64_t smem_start; // Physical address of framebuffer
+    uint32_t smem_len;   // Total memory size in bytes
+    uint32_t type;
+    uint32_t visual;
+    uint32_t line_length;// Pitch (bytes per scanline)
+};
+
+struct fb_var_screeninfo {
+    uint32_t xres;       // Visible resolution width
+    uint32_t yres;       // Visible resolution height
+    uint32_t bits_per_pixel; // BPP (typically 32)
+};
 
 typedef struct vfs_file_operations {
     int64_t (*read)(struct vfs_node *node, uint64_t offset, uint64_t size, uint8_t *buffer);
@@ -19,6 +34,10 @@ typedef struct vfs_file_operations {
     struct vfs_node* (*readdir)(struct vfs_node *node, uint32_t index);
     struct vfs_node* (*finddir)(struct vfs_node *node, const char *name);
     struct vfs_node* (*create)(struct vfs_node *dir, const char *name, uint32_t flags);
+    
+    // NEW METHODS FOR HARDWARE DEVICES & DRIVERS
+    int (*ioctl)(struct vfs_node *node, uint64_t request, void *arg);
+    int64_t (*mmap)(struct vfs_node *node, uint64_t addr, size_t length, int prot, int flags, int64_t offset);
 } vfs_file_operations_t;
 
 typedef struct vfs_node {
